35ebfbe79701c8beeb91bbd459c72df2ade68bba
gabriel <gabrielffguimaraes@gmail.com>
2023-10-06 11:26:03 -0300

    first commit

---
+++ src/main/java/com/alurafood/deliveryserviceregistry/DeliveryServiceregistryApplication.java
@@ -0,0 +1,13 @@
+package com.alurafood.deliveryserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DeliveryServiceregistryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceregistryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alurafood/deliveryserviceregistry/DeliveryServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurafood.deliveryserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
